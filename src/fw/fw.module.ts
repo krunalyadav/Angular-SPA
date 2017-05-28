31ba9f877474288196a8ef210c5c294d6a337f7c
Krunal Yadav <krunal.yadav18@gmail.com>
2017-05-28 10:23:26 +0530

    configuration, styling, responsiveness

--- src/fw/fw.module.ts
+++ src/fw/fw.module.ts
@@ -7,6 +7,9 @@ import { TitleBarComponent } from './title-bar/title-bar.component';
 import { FrameworkConfigService } from './services/framework-config.service';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { StatusBarComponent } from './status-bar/status-bar.component';
+import { ScreenService } from './services/screen.service';
+import { ScreenLarge } from './directives/screen-large.directive';
+import { ScreenBelowLarge } from './directives/screen-below-large.directive';
 
 @NgModule({
   imports: [
@@ -17,10 +20,13 @@ import { StatusBarComponent } from './status-bar/status-bar.component';
     ContentComponent,
     TitleBarComponent,
     TopBarComponent,
-    StatusBarComponent
+    StatusBarComponent,
+    ScreenLarge,
+    ScreenBelowLarge
   ],
   providers: [
-    FrameworkConfigService
+    FrameworkConfigService,
+    ScreenService
   ],
   exports: [FrameworkBodyComponent]
 })
